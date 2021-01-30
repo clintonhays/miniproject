15806cd18c3951126c3c1d9e3f0e6318eff9fa6f
clintonhays <clintonhays84@gmail.com>
2021-01-30 19:18:52 +0900

    add styling @1400, various styling fixes

--- js/app.js
+++ js/app.js
@@ -2,15 +2,22 @@
 
 const nextArrow = document.getElementById("arrowNext");
 const prevArrow = document.getElementById("arrowPrev");
+const pip = document.querySelectorAll(".testimonials__counter_pip");
 
 nextArrow.addEventListener("click", () => {
   const cards = document.querySelector(".testimonial__cards");
 
   cards.style.transform = "translate(-50%)";
+
+  pip[0].classList.remove("active");
+  pip[1].classList.add("active");
 });
 
 prevArrow.addEventListener("click", () => {
   const cards = document.querySelector(".testimonial__cards");
 
   cards.style.transform = "translate(0%)";
+
+  pip[1].classList.remove("active");
+  pip[0].classList.add("active");
 });
